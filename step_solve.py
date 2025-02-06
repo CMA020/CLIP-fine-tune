49c5e6d5f5203a23f2a9ae0d968bf9e0ae90fc28
CM Antony <81664460+CMA020@users.noreply.github.com>
2025-02-06 23:53:49 +0530

    Update step_solve.py

--- step_solve.py
+++ step_solve.py
@@ -213,7 +213,7 @@ def reset_checkpoint_epoch(checkpoint_path):
         print(f"No checkpoint found at {checkpoint_path}")
 
 # Usage
-resume_checkpoint = "/content/drive/MyDrive/clip_weights/clip_rf2_ft_epoch_15_WAY6.pt"
+resume_checkpoint = "/content/drive/MyDrive/clip_weights/clip_rf2_ft_epoch_20_NWAY.pt  "
 # reset_checkpoint_epoch(resume_checkpoint)
 
 # Now load the modified checkpoint
@@ -408,7 +408,7 @@ def trainloop():
         validation_losses=0
         # Save checkpoint
         # Save checkpoint
-        if (epoch + 1) % 5 == 0:  # This will save at epochs 5, 10, 15, 20, etc.
+        if (epoch + 1) % 10 == 0:  # This will save at epochs 5, 10, 15, 20, etc.
             checkpoint = {
                 'epoch': epoch,
                 'model': model,
